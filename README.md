525f4c1ac0ea589753377f68a3b7ef0d95317edb
Zhen Tian <zhen.tian@thoughtworks.com>
2019-11-19 22:52:29 +0800

    feat: README

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # check_branch_protection
 a utility check Github repos branch protection status
+
+## Dependencies
+### platform
+Python3
+### packages
+* pyGithub
+* click
+
+## Install
+```shell script
+git clone https://github.com/dawncold/check_branch_protection
+cd check_branch_protection
+virtualenv -p python3 .env
+source .env/bin/activate
+```
+
+## Usage
+```shell script
+Usage: main.py [OPTIONS]
+
+Options:
+  --token TEXT        personal access token with repo privilege  [required]
+  --repo-prefix TEXT  filter repos prefix  [required]
+  --branch TEXT       branch to check  [default: master]
+  --setup             setup basic branch protection (disable force push)
+  --delete            delete branch protection
+  --help              Show this message and exit.
+```
+
+## List not protected repos
+```shell script
+python3 main.py --token TOKEN --repo-prefix PREFIX
+```
+
+## Setup basic branch protection
+```shell script
+python3 main.py --token TOKEN --repo-prefix PREFIX --setup
+```
+
+## Delete branch protection
+```shell script
+python3 main.py --token TOKEN --repo-prefix PREFIX --delete
+```
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from github import Github, GithubException
 
 @click.command()
 @click.option('--token', required=True, help='personal access token with repo privilege')
-@click.option('--repo-prefix', help='filter repos prefix')
-@click.option('--branch', 'branch_to_check', help='branch to check', default='master')
-@click.option('--setup', 'setup_protection', is_flag=True,
-              help='setup basic branch protection (disable force push)')
+@click.option('--repo-prefix', required=True, help='filter repos prefix')
+@click.option('--branch', 'branch_to_check', default='master', help='branch to check', show_default=True)
+@click.option('--setup', 'setup_protection', is_flag=True, help='setup basic branch protection (disable force push)')
 @click.option('--delete', 'delete_protection', is_flag=True, help='delete branch protection')
 def main(token, repo_prefix, branch_to_check, setup_protection, delete_protection):
     if setup_protection and delete_protection:
         print('choose setup or delete, not both')
         exit(-1)
-    print(f'token: ***, repo prefix: {repo_prefix}, branch: {branch_to_check}, '
-          f'setup protection: {setup_protection}, delete protection: {delete_protection}')
+    print(f'token: ***, repo prefix: {repo_prefix}, branch: {branch_to_check}, ')
+    print(f'setup protection: {setup_protection}, delete protection: {delete_protection}')
+
     g = Github(login_or_token=token)
     for repo in g.get_user().get_repos():
         if repo_prefix and not repo.full_name.startswith(repo_prefix):
